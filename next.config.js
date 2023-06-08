39a6d77f95323110757664b4d85538ea66a10a6b
kassahun <kassuhan@gmail.com>
2023-06-08 08:31:03 +0300

    files added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "ttehkpgjikpjymlhwikz.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
