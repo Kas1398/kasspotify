ae5cda59573dd4304d95a96890fad5373037b105
kassahun <kassuhan@gmail.com>
2023-06-08 14:02:49 +0300

    new packages added

--- components/Player.tsx
+++ components/Player.tsx
@@ -4,7 +4,7 @@ import usePlayer from "@/hooks/usePlayer";
 import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import useGetSongById from "@/hooks/useGetSongById";
 
-//import PlayerContent from "./PlayerContent";
+import PlayerContent from "./PlayerContent";
 
 const Player = () => {
     const player = usePlayer();
@@ -12,9 +12,9 @@ const Player = () => {
 
     const songUrl = useLoadSongUrl(song!);
 
-    /*if (!song || !songUrl || !player.activeId) {
+    if (!song || !songUrl || !player.activeId) {
         return null;
-    }*/
+    }
 
     return (
         <div
@@ -28,7 +28,7 @@ const Player = () => {
                 px-4
             "
         >
-            {/*<PlayerContent key={songUrl} song={song} songUrl={songUrl} />*/}
+            <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
             Player content
         </div>
     );
